d1b3733c83fa8d9798cdec719238bb30b5d162a7
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-03-11 09:23:47 +0900

    0311 Solution

---
+++ CLASS2/2775_부녀회장이 될테야.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+dp = []
+
+for i in range(15):
+    dp.append([0 for _ in range(15)])
+    
+
+for i in range(15):
+    dp[0][i] = i
+    
+for j in range(15):
+    dp[j][1] = 1
+
+
+for i in range(1, 15):
+    for j in range(1, 15):
+        dp[i][j] = dp[i][j-1] + dp[i-1][j]
+    
+print(dp[0])    
+print(dp[1])
+print(dp[2])
+    
+for i in range(T):
+    k = int(input())
+    n = int(input())
+    print(dp[k][n])
+
+
